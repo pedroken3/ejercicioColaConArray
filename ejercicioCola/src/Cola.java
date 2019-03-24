6017e2ec2acf4dbb0570736397321d37621252d6
pedroken3 <33911676+pedroken3@users.noreply.github.com>
2019-03-24 10:01:13 +0100

    repaso
    pequeños retoques y ver como funcionan

--- ejercicioCola/src/Cola.java
+++ ejercicioCola/src/Cola.java
@@ -1,4 +1,73 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Cola {
 
-    //hola mundo
+    private Scanner teclado;
+    private int numero;
+    private int cola[];
+    private int contador;
+
+
+    public Cola() {
+        teclado = new Scanner(System.in);
+        cola = new int[2];
+    }
+
+    public int getString() {
+        return numero;
+    }
+
+    public int putElement() {
+        System.out.println("¿Que numero quieres introducir?");
+        numero = teclado.nextInt();
+        cola[contador] = numero;
+        contador++;
+        if (contador == cola.length) {
+            cola = Arrays.copyOf(cola, cola.length + 2);
+        }
+        return getString();
+    }
+
+    public int getElement() {
+        contador--;
+        if (contador <= 0) {
+            System.out.println("La cola esta vacia, introduce un numero primero");
+        }
+        if (contador > 0) {
+            System.out.println("si entra aki resta");
+            cola[contador] = 0;
+        }
+        return getString();
+    }
+
+    public void size(int contador) {
+        System.out.println("La cola tiene " + (contador) + " elementos");
+    }
+
+    public void imprimirCola() {
+        int recorerCola = 0;
+        while (recorerCola != contador) {
+            System.out.println(cola[recorerCola]);
+            recorerCola++;
+        }
+    }
+
 }
+/**
+ * Ender Luis, [22.10.18 18:46]
+ * Crea una interfaz (espero que sepas lo que es a estas alturas)
+ * <p>
+ * Ender Luis, [22.10.18 18:47]
+ * Que se llame Cola y tenga todos los métodos que necesitas
+ * <p>
+ * Ender Luis, [22.10.18 18:47]
+ * -putElement()  (introducir elemento)
+ * -getElement()  (extraer elemento)
+ * -size()        (cuantos elementos tiene)
+ * <p>
+ * Ender Luis, [22.10.18 18:48]
+ * Y luego crea una clase que implemente esa interfaz llamada ColaConArray
+ * <p>
+ * Qué utilice un array para implementar esos metodos
+ **/
--- ejercicioCola/src/ColaConArray.java
+++ ejercicioCola/src/ColaConArray.java
@@ -1,2 +1,60 @@
-public class ColaConArray {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ColaConArray extends Cola {
+
+    private Scanner teclado;
+    private int arrayCola[];
+    private int contador = 0;
+
+    public ColaConArray() {
+        teclado = new Scanner(System.in);
+        arrayCola = new int[2];
+    }
+
+    public void queHacer() {
+        int hacer = - 1;
+        while (hacer != 0) {
+            System.out.println("Introduce con un numero lo que quieras hacer con la cola: ");
+            System.out.println("si quieres añadir un numero pulsa 1");
+            System.out.println("si quieres extraer un numero pulsa 2");
+            System.out.println("si quieres imprimir la cola pulsa 3");
+            System.out.println("si quieres saber la cantidad de numeros que tiene la cola 4");
+            System.out.println("para salir pulsa 0");
+            hacer = teclado.nextInt();
+            if (hacer == 1) {
+                int numeroAIntroducir;
+                putElement();
+                numeroAIntroducir = teclado.nextInt();
+                arrayCola[contador] = arrayCola[numeroAIntroducir];
+                System.out.println("lo que esta haciendo en añadir");
+                System.out.println(arrayCola[contador]);
+                if (contador == arrayCola.length) {
+                    arrayCola = Arrays.copyOf(arrayCola, arrayCola.length + 2);
+                    System.out.println("Array ampliada.");
+                }
+                contador++;
+            }
+            if (hacer == 2) {
+                contador--;
+                int apoyo;
+                getElement();
+                apoyo = teclado.nextInt();
+                arrayCola[contador] = arrayCola[apoyo];
+            }
+            if (hacer == 3) {
+                imprimirCola();
+            }
+            if (hacer == 4) {
+                size(contador);
+            }
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        ColaConArray ejercicioCola = new ColaConArray();
+        ejercicioCola.queHacer();
+    }
 }
